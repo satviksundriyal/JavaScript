deb17a5d45e619dc5d54839dfd173a75c0e3b33d
satviksundriyal <157103527+satviksundriyal@users.noreply.github.com>
2024-01-28 09:39:51 +0000

    Modify and iteration

---
+++ 05_itertion/01_loops.js
@@ -0,0 +1,8 @@
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`Outer loop ${i}`); //
+//     for (let j = 1; j <= 10; j++) {
+//    //console.log(`Inner loop value ${j} and outer loop ${i}`);
+//      console.log(`${i} ${"*"} ${j} ${"= "} ${i*j}`);   
+//     }
+    //ctrl+shift+l(choose multiple value at same time)
+// }
